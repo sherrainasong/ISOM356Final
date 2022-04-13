0fd790a2073a531f0dec028603d9705d941f6f26
unknown <mapleleafhongye@gmail.com>
2022-04-13 16:14:18 -0400

    Test #1

--- ticketmaster.py
+++ ticketmaster.py
@@ -1,1 +1,5 @@
+# To find events based on information input.
 print ('Hi this is ticketmaster! Helping you find events and concerts!')
+
+print ('Location?') # Ask for the location.
+location = input()
