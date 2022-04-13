ef123b600a3c34150429035e8c6652b3e3205c04
unknown <mapleleafhongye@gmail.com>
2022-04-13 12:37:40 -0400

    ??

---
+++ test.py
@@ -0,0 +1,16 @@
+# import sys
+
+# print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who):
+#     greeting = f"Hello {who}."
+#     return greeting
+
+
+# print(greet("World"))
+# print(greet("Scott"))
+
+R = 3 * 5
+print(R)
